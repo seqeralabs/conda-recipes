e924702ea7bbf4e2a1c8afe0c93ca24638fa5d31
Pablo <pablo.aledo@seqera.io>
2022-10-27 13:15:02 +0200

    add recipe for STAR

--- recipes/star/build.sh
+++ recipes/star/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Installing STAR for OSX."
-    mkdir -p $PREFIX/bin
-    cp bin/MacOSX_x86_64/* $PREFIX/bin
-else 
-    echo "Installing STAR for UNIX/Linux."
-    mkdir -p $PREFIX/bin
-    cp bin/Linux_x86_64_static/* $PREFIX/bin
-fi
+
+export CPPFLAGS="-I$PREFIX/include"
+
+cd source
+make STAR CXXFLAGS_SIMD=""
+mkdir -p $PREFIX/bin
+cp STAR $PREFIX/bin
 chmod +x $PREFIX/bin/STAR
