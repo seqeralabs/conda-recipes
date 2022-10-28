11ba3bcdd42a1fe5deed39e0a0f4fa47a29158f8
Pablo <pablo.aledo@seqera.io>
2022-10-28 16:25:04 +0200

    clean

--- recipes/ucsc-bedGraphToBigWig/build.sh
+++ recipes/ucsc-bedGraphToBigWig/build.sh
@@ -10,33 +10,7 @@ export L="${LDFLAGS}"
 (cd kent/src/htslib && make)
 (cd kent/src/jkOwnLib && make)
 (cd kent/src/hg/lib && make)
-
-#cp $PREFIX/lib/libz.so.1 $PREFIX/lib/libz.so
-
 (cd kent/src/utils/bedGraphToBigWig && make)
 
-# set -eu -o pipefail
-# 
-# mkdir -p $PREFIX/bin
-# mkdir -p $PREFIX/lib
-# 
-# mkdir -p build
-# cd build
-# cmake -DCMAKE_BUILD_TYPE=RELEASE \
-#       -DCONDA_BUILD=TRUE \
-#       -DBoost_NO_BOOST_CMAKE=ON \
-#       -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
-#       -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
-#       -DBoost_NO_SYSTEM_PATHS=ON \
-#       -DNO_IPO=TRUE \
-#       ..
-# make VERBOSE=1
-# echo "unit test executable"
-# ./src/unitTests
-# echo "installing"
-# make install CFLAGS="-L${PREFIX}/lib -I${PREFIX}/include"
-# ../tests/unitTests
-# echo "cmake-powered unit test"
-# CTEST_OUTPUT_ON_FAILURE=1 make test
-#./userApps/kent/src/utils/bedGraphToBigWig
-#./userApps/kent/src/utils/bedGraphToBigWig/bedGraphToBigWig.c
+cp bin/bedGraphToBigWig "$PREFIX/bin"
+chmod +x "$PREFIX/bin/bedGraphToBigWig"
