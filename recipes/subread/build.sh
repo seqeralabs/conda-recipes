ca281a05d2f1f61fafcc5679c57c22aaf5c3555a
Pablo <pablo.aledo@seqera.io>
2022-11-02 10:35:55 +0100

    add optimization flags

--- recipes/subread/build.sh
+++ recipes/subread/build.sh
@@ -8,6 +8,7 @@ MAKEFILE=Makefile.Linux
 export C_INCLUDE_PATH=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 export CC=aarch64-conda-linux-gnu-gcc
+CFLAGS="-O3 -mcpu=neoverse-n1"
 make -f $MAKEFILE CC_EXEC="$CC -L$PREFIX/lib -fcommon"
 cd ..
 cp bin/utilities/* $PREFIX/bin
