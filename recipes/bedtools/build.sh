15ca7540210033c7a1f9a6a16921c2bbfdc72d18
Pablo <pablo.aledo@seqera.io>
2022-11-02 09:23:34 +0100

    add optimization flags

--- recipes/bedtools/build.sh
+++ recipes/bedtools/build.sh
@@ -2,4 +2,4 @@
 export CPLUS_INCLUDE_PATH=${PREFIX}/include
 export C_INCLUDE_PATH=${PREFIX}/include
 
-make install prefix=$PREFIX CXX=$CXX CC=$CC LDFLAGS="-L$PREFIX/lib"
+make install prefix=$PREFIX CXX=$CXX CC=$CC LDFLAGS="-L$PREFIX/lib" CXXFLAGS="-O3 -mcpu=neoverse-n1"
