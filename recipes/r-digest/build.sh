1bd509bb7a9caf83f6ebf7d63a389e69ea5d0b20
Pablo <pablo.aledo@seqera.io>
2022-11-24 12:48:27 +0100

    add some changes

--- recipes/r-digest/build.sh
+++ recipes/r-digest/build.sh
@@ -2,13 +2,13 @@
 
 export DISABLE_AUTOBREW=1
 
-#echo 'CXX = g++'                     >  src/Makevars
-#echo 'CXX11 = g++'                   >> src/Makevars
-#echo 'CXX11FLAGS = -O2 -g $(LTO)'    >> src/Makevars
-#echo 'CXX11PICFLAGS = -fPIC'         >> src/Makevars
-#echo 'CXX11STD = -std=c++11'         >> src/Makevars
-echo 'CXX_STD = CXX'         > src/Makevars
-sed -i "s/1f06212fc4eccfc3c1eb9a2738b5926b/$(md5sum src/Makevars | awk '{print $1}')/g" MD5
+mkdir -p ~/.R
+echo -e "CC=$CC
+FC=$FC
+CXX=$CXX
+CXX98=$CXX
+CXX11=$CXX
+CXX14=$CXX" > ~/.R/Makevars
 
 # shellcheck disable=SC2086
 R CMD INSTALL --build . ${R_ARGS}
