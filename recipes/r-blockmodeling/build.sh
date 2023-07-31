e50dacb1dcf6ec8c19601e8a0c2502455e00b9cb
pabloaledo <112545720+pabloaledo@users.noreply.github.com>
2023-07-31 11:30:33 +0200

    Update recipes/r-blockmodeling/build.sh
    Co-authored-by: Alberto Chiusole <1922124+bebosudo@users.noreply.github.com>

--- recipes/r-blockmodeling/build.sh
+++ recipes/r-blockmodeling/build.sh
@@ -4,14 +4,6 @@
 mv DESCRIPTION DESCRIPTION.old
 grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
 
-
-
-
-
-
-
-
-
 mkdir -p ~/.R
 echo -e "CC=$CC
 FC=$FC
@@ -19,14 +11,6 @@ CXX=$CXX
 CXX98=$CXX
 CXX11=$CXX
 CXX14=$CXX" > ~/.R/Makevars
-
-
-
-
-
-
-
-
 
 $R CMD INSTALL --build .
 
